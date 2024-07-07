becf4fe52385e96eba406e753aa1a09a234889ef
antagonist13 <1703shakayaroslaw@gmail.com>
2024-07-07 18:20:44 +0300

    pages + routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WelcomePageComponent,RegistrationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: WelcomePageComponent },
+    { path: 'register', component: RegistrationComponent }
+];
