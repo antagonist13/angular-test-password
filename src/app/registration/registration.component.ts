b5ee597f854aeb90fcc9ee48f1c87422c99ade7e
antagonist13 <1703shakayaroslaw@gmail.com>
2024-07-08 16:17:00 +0300

    finish project

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -4,11 +4,13 @@ import { Router } from '@angular/router';
 import { IregisterForm } from './interface';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, NgxMaskDirective],
+  providers: [provideNgxMask()],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 }) 
@@ -20,10 +22,10 @@ export class RegistrationComponent {
     lastName: new FormControl<string>('', [Validators.required, Validators.minLength(2)]), 
     address: new FormControl<string>('', [Validators.required, Validators.minLength(2)]),
     address2: new FormControl<string>(''),
-    city: new FormControl<string>(''),
-    state: new FormControl<string>(''),
-    postal: new FormControl<string>(''),
-    phone: new FormControl<string>(''),
+    city: new FormControl<string>('', [Validators.required, Validators.minLength(2)]),
+    state: new FormControl<string>('', [Validators.required, Validators.minLength(2)]),
+    postal: new FormControl<string>('', [Validators.required, Validators.minLength(5)]),
+    phone: new FormControl<string>('', [Validators.required, Validators.minLength(10)]),
     email: new FormControl<string>('', [Validators.email]),
   });
 
@@ -33,6 +35,24 @@ export class RegistrationComponent {
   get _lastName() {
         return this.form.get('lastName');
   }
+  get _address() {
+        return this.form.get('address');
+  }
+  get _city() {
+        return this.form.get('city');
+  }
+  get _state() {
+        return this.form.get('state');
+  }
+  get _postal() {
+        return this.form.get('postal');
+  }
+  get _phone() {
+        return this.form.get('phone');
+  }
+  get _email() {
+        return this.form.get('email');
+  }
   
   navigateToWelcomePage() {
     this.router.navigate(['/']);
@@ -41,6 +61,7 @@ export class RegistrationComponent {
     
     if (this.form.valid) {
       console.log(this.form.value);
+      this.form.reset();
     } else {
       alert( "Form is not valid, try again")
     }
